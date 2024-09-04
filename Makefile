cc74e6f9f2d8f3d94d0fc31b01af3119c268ab70
feyre macbook <feyremacbook@MacBook-Pro.local>
2024-09-03 22:26:53 -0400

    test fix2

--- Makefile
+++ Makefile
@@ -7,10 +7,10 @@ OBJS = $(SRCS:%.c=%.o)
 
 
 a1: $(OBJS)
-	$(GCC) $(TESTFLAGS) $(OBJS) -o a1
+	$(GCC) $(OBJS) -o a1
 
-.c.o:
-	$(GCC) $(TESTFLAGS) -c $*.c
+%.o: %.c
+	$(GCC) -c $< -o $@
 
 testall: test1 test2 test3 test4 test5 test6
 
@@ -19,19 +19,19 @@ test1: a1
 	diff -w output1 expected/expected1
 
 test2: a1
-	./a1 inputs/test2 output2
+	./a1 inputs/test2 > output2
 	diff -w output2 expected/expected2
 
 test3: a1
-	./a1 inputs/test3 output3
+	./a1 inputs/test3 > output3
 	diff -w output3 expected/expected3
 
 test4: a1
-	./a1 inputs/test4 output4
+	./a1 inputs/test4 > output4
 	diff -w output4 expected/expected4
 
 test5: a1
-	./a1 inputs/test5 output5
+	./a1 inputs/test5 > output5
 	diff -w output5 expected/expected5
 
 clean: # remove all machine generated files
--- a1.c
+++ a1.c
@@ -6,18 +6,16 @@ void a1(int input)
 
     for (quarters = input / 25; quarters >= 0; quarters--)
     {
-        input -= quarters * 25;
+        int rem_q = input - quarters * 25;
 
         for (dimes = input / 10; dimes >= 0; dimes--)
         {
-            input -= dimes * 10;
+            int rem_d = rem_q - dimes * 10;
             for (nickels = input / 5; nickels >= 0; nickels--)
             {
-                input -= nickels * 5;
-                for (pennies = input; pennies >= 0; pennies--)
-                {
-                    printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", quarters, dimes, nickels, pennies);
-                }
+                int rem_n = rem_d - nickels * 5;
+                pennies = rem_n;
+                printf("%d quarter(s), %d dime(s), %d nickel(s), %d pennie(s)\n", quarters, dimes, nickels, pennies);
             }
         }
     }
